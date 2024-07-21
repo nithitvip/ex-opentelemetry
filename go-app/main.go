b4a533575a392459f8e48ad5770e6673c3cf85ee
Nithit Vipachon <nithti.vip@ascendcorp.com>
2024-07-21 19:02:00 +0700

    initial project

---
+++ go-app/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		log.Fatal(err)
+	} // listen and serve on 0.0.0.0:8080
+}
