59c01f981849e20991ed9d203755d4e7ae2305dd
Nithit Vipachon <nithti.vip@ascendcorp.com>
2024-07-21 22:10:04 +0700

    add opentlemetry for go

--- go-app/main.go
+++ go-app/main.go
@@ -1,20 +1,111 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
+const ScopeName = "go-demo"
+
 func main() {
+	ctx := context.Background()
+
+	//exp, err := newExporter(ctx)
+	exp, err := newHttpExporter(ctx)
+	if err != nil {
+		log.Fatalf("failed to initialize exporter: %v", err)
+	}
+
+	// Create a new tracer provider with a batch span processor and the given exporter.
+	tp := newTraceProvider(exp)
+
+	// Handle shutdown properly so nothing leaks.
+	defer func() { _ = tp.Shutdown(ctx) }()
+
+	otel.SetTracerProvider(tp)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/ping", traceMiddleware(), func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	err := r.Run()
+	err = r.Run()
 	if err != nil {
 		log.Fatal(err)
 	} // listen and serve on 0.0.0.0:8080
+}
+
+func traceMiddleware() gin.HandlerFunc {
+	tracer := otel.GetTracerProvider().Tracer(ScopeName)
+	return func(c *gin.Context) {
+		ctx2, span := tracer.Start(c.Request.Context(), c.FullPath())
+		defer span.End()
+
+		c.Request = c.Request.WithContext(ctx2)
+
+		c.Next()
+
+		status := c.Writer.Status()
+		span.SetStatus(serverStatus(status))
+		if status > 0 {
+			span.SetAttributes(semconv.HTTPResponseStatusCode(status))
+		}
+		if len(c.Errors) > 0 {
+			span.SetAttributes(attribute.String("gin.errors", c.Errors.String()))
+		}
+	}
+
+}
+
+func serverStatus(code int) (codes.Code, string) {
+	if code < 100 || code >= 600 {
+		return codes.Error, fmt.Sprintf("Invalid HTTP status code %d", code)
+	}
+	if code >= 500 {
+		return codes.Error, ""
+	}
+	return codes.Unset, ""
+}
+
+func newExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
+	// Your preferred exporter: console, jaeger, zipkin, OTLP, etc.
+	return stdouttrace.New(stdouttrace.WithPrettyPrint())
+}
+
+func newHttpExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
+	// Your preferred exporter: console, jaeger, zipkin, OTLP, etc.
+	endpoint := otlptracehttp.WithEndpoint("localhost:4318")
+	return otlptracehttp.New(ctx, otlptracehttp.WithInsecure(), endpoint)
+}
+
+func newTraceProvider(exp sdktrace.SpanExporter) *sdktrace.TracerProvider {
+	// Ensure default SDK resources and the required service name are set.
+	r, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("GoDemoService"),
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(r),
+	)
 }
